0224d2d6d60b04fff26ce58247f879ae97baf0ef
eleinelan <elainefernanda893@yahoo.com.br>
2025-10-20 13:01:47 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -30,4 +30,5 @@ def osasco_fluxo():
     return "Script osasco_fluxo.py iniciado!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # pega a porta correta do Render
+    app.run(host="0.0.0.0", port=port)
